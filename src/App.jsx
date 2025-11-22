d5d9c7a4c3f114001be62220ca067723d53cf03f
LPosi <lrposada@yahoo.com.ar>
2025-11-22 16:40:45 -0300

    Corrigiendo errores

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import Home from "./pages/Home";
+import Logo from "./assets/logo.png";
+import "./App.jsx";
+import Counter from "./components/common/Counter";
+
+function App() {
+  return (
+    <>
+      {/*<Header />*/}
+      {/*<Nav />*/}
+
+      <div>
+        {/*<Aside />*/}
+        <Home />
+        <Counter />
+      </div>
+
+      {/*<Footer />*/}
+    </>
+  );
+}
+
+export default App;
