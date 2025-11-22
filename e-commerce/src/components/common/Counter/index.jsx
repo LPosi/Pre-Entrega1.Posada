3cfb9cb92126f363d5267af056082019093f7b48
LPosi <lrposada@yahoo.com.ar>
2025-11-22 14:39:20 -0300

    Agregué contenido a components/Counter

---
+++ e-commerce/src/components/common/Counter/index.jsx
@@ -0,0 +1,11 @@
+const Counter = () => {
+  return (
+    <div>
+      <button>-</button>
+      <input type="text" value={0}></input>
+      <button>+</button>
+    </div>
+  );
+};
+
+export default Counter;
