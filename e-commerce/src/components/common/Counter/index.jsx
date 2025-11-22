d5bf9fbe21af3b93b866ce79c680fd94c818818f
LPosi <lrposada@yahoo.com.ar>
2025-11-22 15:06:21 -0300

    Agregué contenido al index del Counter para que funcione el botón + al incrementar y el botón - al disminuir

--- e-commerce/src/components/common/Counter/index.jsx
+++ e-commerce/src/components/common/Counter/index.jsx
@@ -2,11 +2,23 @@ import { useState } from "react";
 
 const Counter = () => {
   const [num, setNum] = useState(0);
+  const sumar = () => {
+    setNum(num + 1);
+  };
+
+  const restar = () => {
+    setNum(num - 1);
+  };
+
   return (
     <div>
-      <button style={{ backgroundColor: "gray" }}>-</button>
+      <button style={{ backgroundColor: "gray" }} onClick={restar}>
+        -
+      </button>
       <input type="text" value={num}></input>
-      <button style={{ backgroundColor: "gray" }}>+</button>
+      <button style={{ backgroundColor: "gray" }} onClick={sumar}>
+        +
+      </button>
     </div>
   );
 };
